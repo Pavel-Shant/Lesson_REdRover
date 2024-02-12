f319bfdfa14efb7b8f40617b309b5a3d3a8c4997
1988tcyigan <1988tcyigan32rus@gmail.com>
2024-02-12 16:29:26 +0300

    New Lesson_REdRover

---
+++ Leeson_REDROVer.py
@@ -0,0 +1,14 @@
+# def Print(param):
+#     pass
+#
+#
+# Print ("Hello World!"
+# x = 7
+# if x == 6:
+#     print('six')
+# elif x < 6:
+#     print('Less than six')
+# else:
+#     print('Not equal six')
+
+
