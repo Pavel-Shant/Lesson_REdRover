4972812851f652792b879aa3589346f138efbbf3
1988tcyigan <1988tcyigan32rus@gmail.com>
2024-02-21 17:01:18 +0300

    New Lesson_REdRover 2

---
+++ Lesson 2 Redrover.py
@@ -0,0 +1,60 @@
+# def get_sum(a, b) -> int:
+#     return a + b
+
+
+# print(get_sum(5, 7))
+# name = input('Как твое имя?')
+# print('Меня зовут', name)
+
+# print('Какой язык программирования ты изучаешь?')
+# print(name1,'\n',name2,'\n',name3)  #перенос стр
+
+# nam=input()
+# name1=input()
+# name2=input()
+# name3=input()
+#
+# print(name1, name2, name3, sep=nam)
+
+# a = int(input())
+# v = a * a * a
+# s = 6 * (a * a)
+# print(f"Объем = {v}")    # текст + имя переменной
+# print(f"Площадь полной поверхности = {s}")
+
+# a = int(input())  # считаем формулу
+# b = int(input())
+#
+# f = 3*((a+b)*(a+b)*(a+b)) + 275*(b*b) - 127*a - 41
+# print(f)
+
+# num = int(input())
+#
+# num1 = num + 1
+# num2 = num - 1
+#
+# print(f'Следующее за числом {num} число: {num1}')
+# print(f'Для числа {num} предыдущее число: {num2}')
+
+# a = int(input()) # монитор
+# b = int(input()) # системный блок
+# c = int(input()) # клавиатура
+# d = int(input()) # мышь
+#
+# print((a+b+c+d)*3) # цена 3х компов
+
+# a = int(input())
+# b = int(input())
+#
+# print(f"{a} + {b} =", a + b)
+# print(f"{a} - {b} =",a - b)
+# print(f"{a} * {b} =", a * b)
+
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+# an = a1 + d*(n - 1)
+# print(an)
+
+x = int(input())
+print(x, x*2, x*3, x*4, x*5, sep='---')
