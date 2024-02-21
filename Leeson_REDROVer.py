9c79b7411ba942a849519baa1030c0b3cc498610
1988tcyigan <1988tcyigan32rus@gmail.com>
2024-02-21 17:01:18 +0300

    New Lesson_REdRover 2

--- Leeson_REDROVer.py
+++ Leeson_REDROVer.py
@@ -11,4 +11,42 @@
 # else:
 #     print('Not equal six')
 
+# циклы for
+# for i in range(5):  # для строк используем переменную "с" . для цифр "i/ j/ k "
+#     print('hello')
+
+# for c in 'hello':    #строка
+#     print(c)
+
+# for _ in range(5):
+#     print('hello')
+#
+# for c in 'hello':   # вложенный цикл
+#     for i in range(2):
+#         print(i, c)
+# print('конец.')
+
+  #Программа проверки пароля
+# password = 'hello123@1*'
+# is_valid = True
+#
+# for c in password:
+#     if c in '@_*':
+#         is_valid = False
+#         break
+#
+# if is_valid:
+#     print('Пароль хороший!')
+# else:
+#     print('Пароль плохой!')
+
+#    цикл while
+
+# n=10
+#
+# while True:
+#     print(n)
+#     n -=1
+#     if n==0:
+#         break
 
